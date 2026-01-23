193683e4b8c9ea86415f6d7a75ceb2293b83ea77
drodcab268 <danielroca26@gmail.com>
2026-01-23 13:03:34 +0100

    update of comments

--- provision/dns.sh
+++ provision/dns.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# instalar paquetes
+# DNS installation
 apt-get update
 apt-get install -y bind9 bind9-utils bind9-doc
 
-# copiar ficheros
+# Copying configuration files for DNS zones
 cp -v /vagrant/config/dns/named.conf.local /etc/bind
 cp -v /vagrant/config/dns/db.192.168.56 /var/lib/bind
 cp -v /vagrant/config/dns/db.asir-drc.test /var/lib/bind
 
-# reiniciar servicio
 systemctl restart bind9
 systemctl status bind9
--- provision/mail.sh
+++ provision/mail.sh
@@ -3,16 +3,15 @@ set -e
 
 # Non-interactive Postfix installation for Vagrant
 export DEBIAN_FRONTEND=noninteractive
-
-# Preseed Postfix configuration
 echo "postfix postfix/mailname string asir-drc.test" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 
+# Postfix and Dovecot installation
 apt-get update
 apt-get install postfix dovecot-core dovecot-imapd dovecot-pop3d -y
 
 # --------
-# Postfix
+# Postfix config
 # --------
 postconf -e "home_mailbox = Maildir/"
 postconf -e "mynetworks = 127.0.0.0/8, 192.168.57.0/24, 10.112.0.0/16"
@@ -23,7 +22,7 @@ systemctl restart postfix
 systemctl enable postfix
 
 # --------
-# Dovecot
+# Dovecot config
 # --------
 sed -i 's|^#mail_location =.*|mail_location = maildir:~/Maildir|' /etc/dovecot/conf.d/10-mail.conf
 
